10a4cb891571c856e02d0f40f874eca5480c9867
David Herren <herdav.mobile@gmail.com>
2023-05-22 14:09:36 +0200

    Update setGUID.vb

--- scripts/setGUID.vb
+++ scripts/setGUID.vb
@@ -6,24 +6,24 @@ Public Sub AssemblySample()
     ' Get the active assembly.
     Dim oAsmDoc As AssemblyDocument
     oAsmDoc = ThisApplication.ActiveDocument
-    Logger.Info(oAsmDoc.DisplayName)
 
     ' Call the function that does the recursion.
     Call Assembly(oAsmDoc.ComponentDefinition.Occurrences, 1)
 End Sub
- 
+
 Private Sub Assembly(Occurrences As ComponentOccurrences, _
                              Level As Integer)
     ' Iterate through all of the occurrence in this collection.  This
     ' represents the occurrences at the top level of an assembly.
     Dim oOcc As ComponentOccurrence
+	
     For Each oOcc In Occurrences
-        ' Print the name of the current occurrence.
-        Logger.Info(oOcc.Name)
-        Logger.Info("InternalName:" & oOcc.Definition.Document.InternalName)
-        Logger.Info("RevisionID:" & oOcc.Definition.Document.RevisionId)
-        Logger.Info("DatabaseRevisionID:" & oOcc.Definition.Document.DatabaseRevisionId)
-        Logger.Info("ModelGeometryVersion:" & oOcc.Definition.ModelGeometryVersion)
+		 ' Print the name of the current occurrence.
+'	     Logger.Info(oOcc.Name)
+'        Logger.Info("InternalName:" & oOcc.Definition.Document.InternalName)
+'        Logger.Info("RevisionID:" & oOcc.Definition.Document.RevisionId)
+'        Logger.Info("DatabaseRevisionID:" & oOcc.Definition.Document.DatabaseRevisionId)
+'        Logger.Info("ModelGeometryVersion:" & oOcc.Definition.ModelGeometryVersion)
 
         ' Check to see if this occurrence represents a subassembly
         ' and recursively call this function to traverse through it.
@@ -31,4 +31,12 @@ Private Sub Assembly(Occurrences As ComponentOccurrences, _
             Call Assembly(oOcc.SubOccurrences, Level + 1)
         End If
     Next
+	
+    Dim GUID As String
+    GUID = oOcc.Definition.Document.InternalName
+    GUID = Replace(GUID, "{", "")
+    GUID = Replace(GUID, "}", "")
+	
+	iProperties.Value("Custom", "GUID") = GUID
+	InventorVb.DocumentUpdate()
 End Sub
