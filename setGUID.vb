bdf21e388aa5fb9ccdd37dbde4f0426a1078a8ff
David Herren <herdav.mobile@gmail.com>
2023-05-22 12:23:44 +0200

    Create setGUID.vb

---
+++ setGUID.vb
@@ -0,0 +1,34 @@
+Sub Main()
+    Call AssemblySample()
+End Sub
+
+Public Sub AssemblySample()
+    ' Get the active assembly.
+    Dim oAsmDoc As AssemblyDocument
+    oAsmDoc = ThisApplication.ActiveDocument
+    Logger.Info(oAsmDoc.DisplayName)
+
+    ' Call the function that does the recursion.
+    Call Assembly(oAsmDoc.ComponentDefinition.Occurrences, 1)
+End Sub
+ 
+Private Sub Assembly(Occurrences As ComponentOccurrences, _
+                             Level As Integer)
+    ' Iterate through all of the occurrence in this collection.  This
+    ' represents the occurrences at the top level of an assembly.
+    Dim oOcc As ComponentOccurrence
+    For Each oOcc In Occurrences
+        ' Print the name of the current occurrence.
+        Logger.Info(oOcc.Name)
+        Logger.Info("InternalName:" & oOcc.Definition.Document.InternalName)
+        Logger.Info("RevisionID:" & oOcc.Definition.Document.RevisionId)
+        Logger.Info("DatabaseRevisionID:" & oOcc.Definition.Document.DatabaseRevisionId)
+        Logger.Info("ModelGeometryVersion:" & oOcc.Definition.ModelGeometryVersion)
+
+        ' Check to see if this occurrence represents a subassembly
+        ' and recursively call this function to traverse through it.
+        If oOcc.DefinitionDocumentType = kAssemblyDocumentObject Then
+            Call Assembly(oOcc.SubOccurrences, Level + 1)
+        End If
+    Next
+End Sub
