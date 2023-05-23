edbb232b5291250c100253f09820aa61a8c78e63
David Herren <herdav.mobile@gmail.com>
2023-05-23 13:18:13 +0200

    Update and rename getInstanceProperties.vb to instanceProperties.vb

--- scripts/instanceProperties.vb
+++ scripts/instanceProperties.vb
@@ -1,5 +1,6 @@
 Sub Main()
 	getInstanceProperties()
+	setInstanceProperties()
 End Sub
 
 Sub getInstanceProperties()
@@ -17,5 +18,23 @@ Sub getInstanceProperties()
 		
 		' Print the component name
 		Logger.Info(componentName & ": " & iProperties.InstanceValue(componentName, instanceName))
+		iProperties.InstanceValue(componentName, "iName") = componentName
+    Next
+End Sub
+
+Sub setInstanceProperties()
+	Dim instanceName = "iName"
+    ' Get the active assembly document
+    Dim asmDoc As AssemblyDocument
+    asmDoc = ThisApplication.ActiveDocument
+    
+    ' Iterate through all occurrences in the assembly
+    Dim occ As ComponentOccurrence
+    For Each occ In asmDoc.ComponentDefinition.Occurrences
+        ' Get the name of the component
+        Dim componentName As String
+        componentName = occ.Name
+
+		iProperties.InstanceValue(componentName, instanceName) = componentName
     Next
 End Sub
